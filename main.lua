586d77f397e47c5ef3e6e46f6f9d59a875108b3d
alfredoChi <conomia_alfredo@hotmail.com>
2017-01-10 11:52:40 -0500

    inicio de la app

--- main.lua
+++ main.lua
@@ -21,7 +21,7 @@ if not isUser then
 	})
 else
 	--composer.gotoScene( "src.Visits" )
-	composer.gotoScene( "src.LoginFacebook", {
+	composer.gotoScene( "src.Home", {
 		time = 400,
 		effect = "crossFade",
 		params = { id = 20 }
